2a555e8d932d10d1adbf1861a5687c981bf402d8
Alexander Mostovtsov <mostovtsov@gmail.com>
2016-12-02 14:45:44 +0300

    adding and saving notes

--- notes.js
+++ notes.js
@@ -1,10 +1,28 @@
 console.log('Starting app.js');
-//console.log(module);
 
-module.exports.age = 28;
+const fs = require('fs');
 
 var addNote = (title, body) => {
-    console.log('Adding note', title, body)
+    //console.log('Adding note', title, body)
+    var notes = [];
+    var note = {
+        title,
+        body
+    };
+
+    try {
+        var notesString = fs.readFileSync('notes-data.json');
+        notes = JSON.parse(notesString);
+    } catch (e) {
+
+    }
+
+    var duplicatesNotes = notes.filter((note) => note.title === title);
+
+    if (duplicatesNotes.length === 0) {
+        notes.push(note);
+        fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+    }
 };
 
 var readNote = (title) => {
