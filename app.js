282178738bd7bb9a882afbdec8f8a6f36efe0434
Alexander Mostovtsov <mostovtsov@gmail.com>
2016-12-02 13:22:25 +0300

    work with json

--- app.js
+++ app.js
@@ -2,16 +2,13 @@ console.log('Starting app');
 
 const fs = require('fs');
 const yargs = require('yargs');
-//const os = require('os');
 const _ = require('lodash');
 const notes = require('./notes.js');
 
 const argv = yargs.argv;
 
 var command = argv._[0];
-//console.log('Command: ', command);
 console.log('Yargs: ', argv);
-//console.log(process.argv);
 
 
 if (command === 'add') {
@@ -25,24 +22,3 @@ if (command === 'add') {
 } else {
     console.log('Command not recognized');
 }
-
-// var res = notes.addNote();
-
-// console.log(_.isString(true));
-// console.log(_.isString('Sasha'));
-
-
-// var filteredArray = _.uniq(['Kolia', 1, 'Sasha',1,2,3,4]);
-
-// console.log(filteredArray);
-
-// console.log(res);
-
-
-// console.log('Result:', notes.add(9, -2));
-
-// var user = os.userInfo();
-
-// fs.appendFile('greetings.txt', `Hello ${user.username}! Yoy are ${notes.age}.`);
-
-
