cf0ba48a9508da5a0c25bcc7025b10087afc2b22
Alexander Mostovtsov <mostovtsov@gmail.com>
2016-12-05 12:08:03 +0300

    callback errors

--- Weather-app/app.js
+++ Weather-app/app.js
@@ -1,6 +1,8 @@
 const request = require('request');
 const yargs = require('yargs');
 const apiEndPoint = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
+const statusNotFound = 'ZERO_RESULTS';
+const statusOK = 'OK';
 
 const argv = yargs
     .options({
@@ -23,7 +25,13 @@ request({
     url: apiEndPoint + inputAddress,
     json: true
 }, (error, response, body) => {
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`lat ${body.results[0].geometry.location.lat}`);
-    console.log(`lng ${body.results[0].geometry.location.lng}`);
+    if (error) {
+        console.log('Unable to connect to Google servers.')
+    } else if (body.status === statusNotFound) {
+        console.log('Unbale to find that address');
+    } else if (body.status === statusOK) {
+        console.log(`Address: ${body.results[0].formatted_address}`);
+        console.log(`lat ${body.results[0].geometry.location.lat}`);
+        console.log(`lng ${body.results[0].geometry.location.lng}`);
+    }
 });
