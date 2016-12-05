d3cb472b8da1f45b81d909826db47125c991b791
Alexander Mostovtsov <mostovtsov@gmail.com>
2016-12-05 11:57:45 +0300

    encode user input

--- Weather-app/app.js
+++ Weather-app/app.js
@@ -1,8 +1,29 @@
 const request = require('request');
+const yargs = require('yargs');
+const apiEndPoint = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
+
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for',
+            string: true
+        }
+    }).help()
+    .alias('help', 'h')
+    .argv;
+//Perevalochnaya 25 Gomel
+
+console.log(argv);
+
+var inputAddress = encodeURIComponent(argv.address);
 
 request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
+    url: apiEndPoint + inputAddress,
     json: true
 }, (error, response, body) => {
-    console.log(body);
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`lat ${body.results[0].geometry.location.lat}`);
+    console.log(`lng ${body.results[0].geometry.location.lng}`);
 });
