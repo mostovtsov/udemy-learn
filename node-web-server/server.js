a956c4c93672be3c534f4215597c8fc7136a0b88
Alexander Mostovtsov <mostovtsov@gmail.com>
2016-12-06 11:52:08 +0300

    express middleware

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,17 +1,35 @@
 const express = require('express');
 const hbs = require('hbs');
-
+const fs = require('fs');
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
+
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('service.log', log + '\n', (err) => {
+        if (err) {
+            console.log(err);
+        }
+    })
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
+
 app.use(express.static(__dirname + '/public'));
 
-hbs.registerHelper('getCurrentYear', ()=> {
+hbs.registerHelper('getCurrentYear', () => {
     return new Date().getFullYear();
 });
 
-hbs.registerHelper('screamIt', (text)=> {
+hbs.registerHelper('screamIt', (text) => {
     return text.toUpperCase();
 });
 
@@ -35,6 +53,6 @@ app.get('/bad', (req, res) => {
     })
 });
 
-app.listen(3000, ()=>{
+app.listen(3000, () => {
     console.log('server is up on port 3000');
 });
