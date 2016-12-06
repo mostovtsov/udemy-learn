92fc8bcb9cf7c2d1e8a328c61e8c3ff98e32f3ea
Alexander Mostovtsov <mostovtsov@gmail.com>
2016-12-06 11:14:28 +0300

    rendering temlates with data (handlebars)

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,20 +1,25 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
 
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 
 app.get('/', (req, res) => {
-    //res.send('<h1>Hello Express!</h1>');
-    res.send({
-        name: 'Sasha',
-        likes: ['Biking', 'Girls']
-    })
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        currentYear: new Date().getFullYear(),
+        welcomeText: 'Nice to see you!'
+    });
 });
 
 app.get('/about', (req, res) => {
-    res.send('About page')
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
+    });
 });
 
 app.get('/bad', (req, res) => {
