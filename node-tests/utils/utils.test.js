45b97d3a68600c8e8df9c4d0f022519b01b3554c
Alexander Mostovtsov <mostovtsov@gmail.com>
2016-12-07 12:10:26 +0300

    using assertion library

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,19 +1,43 @@
+const expect = require('expect');
 const utils = require('./utils');
 
-it('sould add two numbers', () => {
+it('should add two numbers', () => {
     var res = utils.add(33, 11);
     var expected = 44;
-    if (res !== expected) {
-        throw new Error(`Expected ${expected}, but got ${res}`);
-    }
 
+    expect(res).toBe(expected).toBeA('number');
 });
 
-it('sould square a number', () => {
+it(`should square a number`, () => {
     var res = utils.square(10);
     var expected = 100;
-    if (res !== expected) {
-        throw new Error(`Expected ${expected}, but got ${res}`);
-    }
 
+    expect(res).toBe(expected).toBeA('number');
 });
+
+// it('should expect some values', () => {
+//     //expect({ name: 'Sasha' }).toEqual({ name: 'Sasha' });
+//     //expect([2,3,4]).toInclude(3);
+
+
+//     expect({
+//         name: 'Sasha',
+//         age: 28
+//     }).toInclude({
+//         age: 28
+//     })
+// });
+
+
+it('should verify first and last names are set', () => {
+    var user = { firstName: '', lastName: '' };
+    var res = utils.setName(user, 'Alexander Mostovtsov');
+
+    expect(res).toInclude({
+        firstName: 'Alexander',
+        lastName: 'Mostovtsov'
+    })
+})
+
+
+
