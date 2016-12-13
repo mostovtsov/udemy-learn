97cc3b07e150b712cb85c27fc0909850473cca39
IIIypik <mostovtsov@gmail.com>
2016-12-13 11:50:23 +0300

    mongoose + save

---
+++ node-todo-api/server/server.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+var Todo = mongoose.model('Todo', {
+    text: {
+        type: String
+    },
+    completed: {
+        type: Boolean
+    },
+    completedAt: {
+        type: Number
+    }
+});
+
+var newTodo = new Todo({
+    text: 'Cook dinner'
+});
+
+var newTodo = new Todo({
+    text: 'Play dota',
+    completed: false,
+    completedAt: 14
+});
+
+newTodo.save().then((doc) => {
+    console.log('Saved todo', doc);
+}, (e) => {
+    console.log('Unable to save todo');
+})
